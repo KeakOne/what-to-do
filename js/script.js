77f15ed381d68e7193f495d51a4ac98ca56adcfc
Keakone <keanucarnevale@gmail.com>
2016-03-09 07:00:22 +1300

    created basic styling, added content to index

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$("h3").html("<i>jQuery Playground</i>");
+
+
+
+$(document).ready(function() {
+
+$("#containerbutton").click(function(){
+$('#container').css({"background-color":"dodgerblue"});
+});
+
+
+});
